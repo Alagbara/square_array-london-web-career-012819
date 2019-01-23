6eebd0d366700cdf6c0e1553865749224fd54b8d
Moses Olawoyin <m.bastford@gmail.com>
2019-01-23 00:31:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
 
 def square_array(array)
-new_array = []
-array.each do |num|
-new_array.push num*num
-end
-return new_array
+  new_array = []
+  array.each do |num|
+  new_array.push num*num
+  end
+    return new_array
 
 end
